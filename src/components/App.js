d54dd049bf77ebc515395c60e7c7db2eb79f6e07
timothy <timothy.ndarau@student.moringaschool.com>
2023-07-19 14:41:32 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,24 @@
 import React from "react";
 import BlogPost from "./BlogPost";
+import Header from "./header.js";
+
+console.log(BlogPost);
+
 
 function App() {
-  return <BlogPost />;
-}
+
+
+  return (
+    <>
+    <header name = "rizz"/>
+    <header name = "gabe"/>
+
+    <div className="App">
+      you are good
+
+    </div>
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Header(Props){
+    return(
+        <h1>my name is: {Props.name} and i am {Props.age}</h1>
+    )
+}
+
+export default Header 
